c460f3ff42d68a20c32fd21fb4d6031a2e49b789
杨威 <yangwei@37ren.com>
2016-08-08 19:36:05 +0800

    Initialize

---
+++ Assets/Editor/XAnimationExport.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+public class XAnimationDetial : EditorWindow
+{
+	public AnimationClip animClip;
+	public GameObject targetObj;
+
+	public AnimationCurve xPosCurve;
+	public AnimationCurve yPosCurve;
+	public AnimationCurve zPosCurve;
+
+	void OnGUI()
+	{
+		animClip = EditorGUILayout.ObjectField(animClip, typeof(AnimationClip), false) as AnimationClip;
+		targetObj = EditorGUILayout.ObjectField(targetObj, typeof(GameObject), true) as GameObject;
+		if (GUILayout.Button("Process"))
+		{
+			ProcessAnimClip();	
+		}
+
+		if (xPosCurve != null)
+		{
+			EditorGUILayout.CurveField(xPosCurve);
+		}
+		if (yPosCurve != null)
+		{
+			EditorGUILayout.CurveField(yPosCurve);
+		}
+		if (zPosCurve != null)
+		{
+			EditorGUILayout.CurveField(zPosCurve);
+		}
+	}
+
+	void ProcessAnimClip()
+	{
+		EditorCurveBinding[] curveBindingArray = AnimationUtility.GetCurveBindings(animClip);
+		XAnimMove animMove = targetObj.GetComponentInParent<XAnimMove>();
+		animMove.animLength = animClip.length;
+		foreach (var curveBinding in curveBindingArray)
+		{
+			Object animObj = AnimationUtility.GetAnimatedObject(targetObj, curveBinding);
+			AnimationCurve curve = AnimationUtility.GetEditorCurve(animClip, curveBinding);
+			if (animObj.name == "Bip001")
+			{
+				if (curveBinding.propertyName.Contains("m_LocalPosition.x"))
+				{					
+					animMove.xPosCurve = xPosCurve = curve;
+				}
+				else if (curveBinding.propertyName == "m_LocalPosition.y")
+				{
+					animMove.yPosCurve = yPosCurve = curve;
+				}
+				else if (curveBinding.propertyName == "m_LocalPosition.z")
+				{
+					animMove.zPosCurve = zPosCurve = curve;
+				}
+				Debug.LogFormat("animObjName:{0}, path:{1}, propertyName:{2}, keyFrameCount:{3}", 
+				                animObj.name, 
+				                curveBinding.path,
+				                curveBinding.propertyName,
+				                curve.keys.Length);
+			}
+		}
+	}
+}
---
+++ Assets/_scripts/XAnimMove.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XAnimMove : MonoBehaviour 
+{
+	public float ratio = 1f;
+	public Animator animator;
+
+	public float animLength;
+	public AnimationCurve xPosCurve;
+	public AnimationCurve yPosCurve;
+	public AnimationCurve zPosCurve;
+
+	float lastEvaluateTime = 0f;
+
+	float _startTime;
+
+	float lastPos = 0;
+
+	public void CallStart()
+	{
+		_startTime = Time.time;
+		lastPos = 0;
+	}
+
+	public void CallUpdate () 
+	{
+		float time = Time.time;
+		while (time > animLength)
+		{
+			time -= animLength;
+		}
+
+		Vector3 pos = transform.position;
+		float curPos = zPosCurve.Evaluate(time);
+		pos.z += curPos - lastPos;
+		transform.position = pos;
+		lastPos = curPos;
+
+//		float normalizedTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+//
+//		while (normalizedTime > 1)
+//		{			
+//			normalizedTime -= 1;
+//		}
+//
+//		float evaluateTime = normalizedTime * animLength;
+//
+//		if (evaluateTime - lastEvaluateTime > 0)
+//		{
+//			Vector3 pos = transform.position;
+//			pos.z += (zPosCurve.Evaluate(evaluateTime) - zPosCurve.Evaluate(lastEvaluateTime)) * ratio;
+//			transform.position = pos;
+//
+//			Debug.LogWarning((zPosCurve.Evaluate(evaluateTime) - zPosCurve.Evaluate(lastEvaluateTime)) * ratio);
+//
+//			lastEvaluateTime = evaluateTime;
+//		}
+//		else
+//		{
+//			lastEvaluateTime = 0;
+//		}
+	}
+}
